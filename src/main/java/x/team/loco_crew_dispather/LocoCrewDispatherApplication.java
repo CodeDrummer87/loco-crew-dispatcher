b9f0be390f62ac63e30f6b3660d25e469be72a9e
Andrey Bodrov <code.drummer87@gmail.com>
2026-03-04 01:50:12 +0600

    the project has been initialized

---
+++ src/main/java/x/team/loco_crew_dispather/LocoCrewDispatherApplication.java
@@ -0,0 +1,13 @@
+package x.team.loco_crew_dispather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocoCrewDispatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocoCrewDispatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/x/team/loco_crew_dispather/LocoCrewDispatherApplicationTests.java
@@ -0,0 +1,13 @@
+package x.team.loco_crew_dispather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocoCrewDispatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
