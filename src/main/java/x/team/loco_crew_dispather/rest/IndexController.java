13d9140782a11275f1d32faf8f86510dfbc21589
kurbatovss <kssdevspace@gmail.com>
2026-03-09 19:54:27 +0300

    index page created

--- src/main/java/x/team/loco_crew_dispather/rest/IndexController.java
+++ src/main/java/x/team/loco_crew_dispather/rest/IndexController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class IndexController {
 
-    @GetMapping("/login.html")
-    public String login() {
+    @GetMapping("/index.html")
+    public String index() {
         //Возвращаем путь к HTML-файлу внутри папки static
-        return "forward:/html/login.html";
+        return "forward:/html/index.html";
     }
 }
