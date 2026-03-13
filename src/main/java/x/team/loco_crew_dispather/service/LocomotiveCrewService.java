b72f72cbd79f97c0b5853867ab67b1283b854b33
Dmitry Bobrov <bobrowww@mail.ru>
2026-03-13 19:35:14 +0500

    created locomotive crew service

---
+++ src/main/java/x/team/loco_crew_dispather/service/LocomotiveCrewService.java
@@ -0,0 +1,26 @@
+package x.team.loco_crew_dispather.service;
+
+import org.springframework.stereotype.Service;
+import x.team.loco_crew_dispather.dto.LocomotiveCrewDto;
+import x.team.loco_crew_dispather.repository.LocomotiveCrewRepository;
+
+import java.util.List;
+
+@Service
+public class LocomotiveCrewService {
+
+    private final LocomotiveCrewRepository locomotiveCrewRepository;
+
+    public LocomotiveCrewService(LocomotiveCrewRepository locomotiveCrewRepository) {
+        this.locomotiveCrewRepository = locomotiveCrewRepository;
+    }
+
+    public long getLocomotiveCrewCount(){
+        return locomotiveCrewRepository.count();
+    }
+
+    public List<LocomotiveCrewDto> getLocomotiveCrewList(){
+        return locomotiveCrewRepository.getAllLocomotiveCrews();
+    }
+}
+
