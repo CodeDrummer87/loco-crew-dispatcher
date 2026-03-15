7f95b4b7302aeddfd7a588494b59f919332e1eab
kurbatovss <kssdevspace@gmail.com>
2026-03-15 12:39:45 +0300

    endpoint for getting all employees created

---
+++ src/main/java/x/team/loco_crew_dispather/dto/EmployeeDto.java
@@ -0,0 +1,18 @@
+package x.team.loco_crew_dispather.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class EmployeeDto {
+
+    private Long id;
+    private String fullName;
+    private Integer personnelNumber;
+    private String positionTitle;
+    private String positionAbbreviate;
+}
