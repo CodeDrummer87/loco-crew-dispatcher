213c7ee10d9a7b564a65614f95eeb9906749a7d2
Dmitry Bobrov <bobrowww@mail.ru>
2026-03-15 14:33:54 +0500

    request error case handled

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,7 +2,20 @@
 const locomotive_crew_count = document.getElementById('s_locomotive_crew_count');
 
 document.addEventListener('DOMContentLoaded', async function () {
-    const response = await fetch('http://localhost:8080/api/v1/locomotive-crews/count');
-    locomotive_crew_count.innerText = await response.json();
+    await getLocomotiveCrewCount();
 });
+
+async function getLocomotiveCrewCount() {
+    try {
+        const response = await fetch('http://localhost:8080/api/v1/locomotive-crews/count');
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error! status: ${response.status}`);
+        }
+
+        locomotive_crew_count.innerText = await response.json();
+    } catch (error) {
+        console.error("Ошибка выполнения запроса (счётчик локомотивных бригад): " + error);
+    }
+}
 //endregion
