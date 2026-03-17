ba5938881ad7243b52008c39d29ee85e0817962e
kurbatovss <kssdevspace@gmail.com>
2026-03-17 11:39:57 +0300

    frontend request for employee counter created

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,9 @@
 const locomotive_crew_count = document.getElementById('s_locomotive_crew_count');
+const employee_count = document.getElementById("s_employee_count");
 
 document.addEventListener('DOMContentLoaded', async function () {
     await getLocomotiveCrewCount();
+    await getEmployeeCount();
 });
 
 async function getLocomotiveCrewCount() {
@@ -15,5 +17,19 @@ async function getLocomotiveCrewCount() {
         locomotive_crew_count.innerText = await response.json();
     } catch (error) {
         console.error("Ошибка выполнения запроса (счётчик локомотивных бригад): " + error);
+    }
+}
+
+async function getEmployeeCount() {
+    try {
+        const response = await fetch('http://localhost:8080/api/v1/employees/count');
+
+        if (!response.ok) {
+            throw new Error(`HTTP Error! status: ${response.status}`);
+        }
+
+        employee_count.innerText = await response.json();
+    } catch (error) {
+        console.error("Ошибка выполнения запроса (счётчик сотрудников): " + error);
     }
 }
